ad10e32ac2e5cb5e3ac01375ac54228b1d01fd89
Sven Tiigi <sven.tiigi@gmail.com>
2023-01-20 09:56:27 +0100

    Use europe-west3 region

--- functions/src/firestore/calculate-carbon-emission.firestore.ts
+++ functions/src/firestore/calculate-carbon-emission.firestore.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 
 export default functions
-  .region("europe-west1")
+  .region("europe-west3")
   .firestore.document("users/{userId}/consumptions/{consumptionId}")
   .onWrite(async (snapshot, context) => {
     // TODO: Calculate and Update Carbon Emission
