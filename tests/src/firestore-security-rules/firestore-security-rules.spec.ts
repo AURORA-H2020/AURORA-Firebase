04f822d04403bacba530e604e87e82e39a0e5d39
Sven Tiigi <sven.tiigi@gmail.com>
2023-01-20 17:46:51 +0100

    Updated Firestore Security Rules Tests

--- tests/src/firestore-security-rules/firestore-security-rules.spec.ts
+++ tests/src/firestore-security-rules/firestore-security-rules.spec.ts
@@ -9,6 +9,7 @@ import * as fs from "fs";
 
 describe("Firestore Security Rules", () => {
   let testEnvironment: RulesTestEnvironment;
+  let authenticatedContext: RulesTestContext;
   let unauthenticatedContext: RulesTestContext;
 
   beforeEach(async () => {
@@ -18,23 +19,36 @@ describe("Firestore Security Rules", () => {
         rules: fs.readFileSync("../firestore.rules", "utf8"),
       },
     });
+    authenticatedContext = testEnvironment.authenticatedContext("USER_ID");
     unauthenticatedContext = testEnvironment.unauthenticatedContext();
   });
 
   afterEach(() => testEnvironment.cleanup());
 
-  describe("Unauthorized User", () => {
-    describe("sites Collection", () => {
+  describe("/sites", () => {
+    describe("Unauthorized User", () => {
       it("Deny to read a single site", async () =>
         await assertFails(unauthenticatedContext.firestore().collection("sites").doc("1").get()));
-      it("Allow to list all sites", async () =>
-        await assertSucceeds(unauthenticatedContext.firestore().collection("sites").get()));
+      it("Deny to list all sites", async () =>
+        await assertFails(unauthenticatedContext.firestore().collection("sites").get()));
       it("Deny to create a site", async () =>
         await assertFails(unauthenticatedContext.firestore().collection("sites").add({})));
       it("Deny to update a site", async () =>
         await assertFails(unauthenticatedContext.firestore().collection("sites").doc("1").update({})));
       it("Deny to delete a site", async () =>
         await assertFails(unauthenticatedContext.firestore().collection("sites").doc("1").delete()));
+    });
+    describe("Authorized User", () => {
+      it("Deny to read a single site", async () =>
+        await assertFails(authenticatedContext.firestore().collection("sites").doc("1").get()));
+      it("Allow to list all sites", async () =>
+        await assertSucceeds(authenticatedContext.firestore().collection("sites").get()));
+      it("Deny to create a site", async () =>
+        await assertFails(authenticatedContext.firestore().collection("sites").add({})));
+      it("Deny to update a site", async () =>
+        await assertFails(authenticatedContext.firestore().collection("sites").doc("1").update({})));
+      it("Deny to delete a site", async () =>
+        await assertFails(authenticatedContext.firestore().collection("sites").doc("1").delete()));
     });
   });
 });
