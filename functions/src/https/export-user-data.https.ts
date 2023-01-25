6bbe183b14beefe258303c16fcbf5659c5a28421
Sven Tiigi <sven.tiigi@gmail.com>
2023-01-25 15:43:51 +0100

    Implemented export-user-data Cloud Function

--- functions/src/https/export-user-data.https.ts
+++ functions/src/https/export-user-data.https.ts
@@ -1,7 +1,23 @@
 import { region } from "firebase-functions";
-import { preferredRegion } from "../constants";
+import { consumptionsCollectionName, preferredRegion, usersCollectionName } from "../constants";
+import { HttpsError } from "firebase-functions/lib/common/providers/https";
+import { firestore } from "firebase-admin";
+import * as Path from "path";
 
 export default region(preferredRegion).https.onCall(async (data, context) => {
-  // TODO: Retrieve all User Data and send an export to the mail address
-  console.log(data, context, context.auth?.token.email);
+  const auth = context.auth;
+  if (!auth) {
+    throw new HttpsError("failed-precondition", "The function must be called while authenticated");
+  }
+  const user = (await firestore().doc(Path.join(usersCollectionName, auth.uid)).get()).data();
+  if (!user) {
+    throw new HttpsError("not-found", "No user data available");
+  }
+  const consumptions = (
+    await firestore().collection(Path.join(usersCollectionName, auth.uid, consumptionsCollectionName)).get()
+  ).docs.map((doc) => doc.data());
+  return {
+    user: user,
+    consumptions: consumptions,
+  };
 });
