1e34a78c32b77326386f4803232523baecaa5274
Sven Tiigi <sven.tiigi@gmail.com>
2023-01-25 17:33:49 +0100

    Fixed import

--- functions/src/https/download-user-data.https.ts
+++ functions/src/https/download-user-data.https.ts
@@ -1,6 +1,5 @@
-import { region } from "firebase-functions";
+import { region, https } from "firebase-functions";
 import { consumptionsCollectionName, preferredRegion, usersCollectionName } from "../constants";
-import { HttpsError } from "firebase-functions/lib/common/providers/https";
 import { firestore } from "firebase-admin";
 import * as Path from "path";
 
@@ -8,14 +7,14 @@ export default region(preferredRegion).https.onCall(async (data, context) => {
   // Check if auth is not available
   if (!context.auth) {
     // Throw failed precondition error
-    throw new HttpsError("failed-precondition", "The function must be called while authenticated");
+    throw new https.HttpsError("failed-precondition", "The function must be called while authenticated");
   }
   // Retrieve user data
   const user = (await firestore().doc(Path.join(usersCollectionName, context.auth.uid)).get()).data();
   // Check if user data is unavailable
   if (!user) {
     // Throw not found error
-    throw new HttpsError("not-found", "No user data available");
+    throw new https.HttpsError("not-found", "No user data available");
   }
   // Retrieve consumptions data
   const consumptions = (
