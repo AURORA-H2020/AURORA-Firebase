ae6262e219cc37ced6d7eff64fae8e1ca682244c
Sven Tiigi <sven.tiigi@gmail.com>
2023-01-26 08:14:23 +0100

    Added runWith operator

--- functions/src/https/download-user-data.ts
+++ functions/src/https/download-user-data.ts
@@ -13,29 +13,35 @@ initializeAppIfNeeded();
  * This functions aggregates all Firestore documents which are associated with the user
  * and responds a JSON object containing the relevant information.
  */
-export const downloadUserData = functions.region(preferredRegion).https.onCall(async (data, context) => {
-  // Check if auth is not available
-  if (!context.auth) {
-    // Throw failed precondition error
-    throw new functions.https.HttpsError("failed-precondition", "The function must be called while authenticated");
-  }
-  // Retrieve user data
-  const user = (await admin.firestore().doc(Path.join(usersCollectionName, context.auth.uid)).get()).data();
-  // Check if user data is unavailable
-  if (!user) {
-    // Throw not found error
-    throw new functions.https.HttpsError("not-found", "No user data available");
-  }
-  // Retrieve consumptions data
-  const consumptions = (
-    await admin
-      .firestore()
-      .collection(Path.join(usersCollectionName, context.auth.uid, consumptionsCollectionName))
-      .get()
-  ).docs.map((doc) => doc.data());
-  // Return data
-  return {
-    user: user,
-    consumptions: consumptions,
-  };
-});
+export const downloadUserData = functions
+  .region(preferredRegion)
+  .runWith({
+    // TODO: Enable as soon AppCheck is enabled in the apps
+    enforceAppCheck: false,
+  })
+  .https.onCall(async (data, context) => {
+    // Check if auth is not available
+    if (!context.auth) {
+      // Throw failed precondition error
+      throw new functions.https.HttpsError("failed-precondition", "The function must be called while authenticated");
+    }
+    // Retrieve user data
+    const user = (await admin.firestore().doc(Path.join(usersCollectionName, context.auth.uid)).get()).data();
+    // Check if user data is unavailable
+    if (!user) {
+      // Throw not found error
+      throw new functions.https.HttpsError("not-found", "No user data available");
+    }
+    // Retrieve consumptions data
+    const consumptions = (
+      await admin
+        .firestore()
+        .collection(Path.join(usersCollectionName, context.auth.uid, consumptionsCollectionName))
+        .get()
+    ).docs.map((doc) => doc.data());
+    // Return data
+    return {
+      user: user,
+      consumptions: consumptions,
+    };
+  });
