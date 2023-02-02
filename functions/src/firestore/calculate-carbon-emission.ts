236f3123913987562f72f4c53b6be7a70f5ca1f4
LarsLrn <92923361+LarsLrn@users.noreply.github.com>
2023-02-02 16:28:26 +0100

    Added carbon emission calculation for electricity (testing) (#6)
    * Update Cloud Function to respect consumption category
    
    * Updated Carbon Emission Calculation cloud function
    
    * Added electricity carbon footprint calculations
    
    * Fixed ES Lint Errors

--- functions/src/firestore/calculate-carbon-emission.ts
+++ functions/src/firestore/calculate-carbon-emission.ts
@@ -38,20 +38,83 @@ async function carbonEmissions(
   // TODO: Correctly calculate carbon emissions
 
   const category: ConsumptionCategory = snapshot.after.data()?.category
-  let carbonEmission = 0.1
+  const value = snapshot.after.data()?.category
+  let carbonEmission = 0 // This can probably be removed, as there should be no scenario where a value other than electricity, transportation or heating is set as the category.
 
+  /**
+  * I am unsure how to get values from the "sites" collection in Firebase, so I just recreated it here.
+  * This will need to be properly integrated.
+  */
+
+  // Copy of the "Sites" collection in Firebase
+  const sites = {
+    "8yancCd0U8IOTE6VzBEa": {
+      site: "Denmark",
+      electricity: 0.116,
+      transportation: 0.1
+    },
+    "YNkAonVX5g9zV2S8Do8b": {
+      site: "Portugal",
+      electricity: 0.201
+    },
+    "JqlDcJN6yaVMOM6CxkSo": {
+      site: "Slovenia",
+      electricity: 0.219
+    },
+    "P2dpcsdZns3514ylEP7U": {
+      site: "Spain",
+      electricity: 0.19
+    },
+    "tR22sHpRRLuEc8R5XQDs": {
+      site: "UK",
+      electricity: 0.209
+    },
+  }
+
+  // This currently does not exist in Firebase. Unsure how it should be implemented, but essentially these values are "Global", so applicable across all sites.
+  /**
+  const emissionFactorsGlobal = {
+    heating: {
+      heatingOil: 0.267,
+      naturalGas: 0.202,
+      liquifiedPetroGas: 0.227,
+      bioMass: 0.118,
+      bioMassLocal: 0,
+      geoThermal: 0.05,
+      solarThermal: 0.0396,
+      districtCoal: 0.354,
+      districtBiomass: 0.118,
+      districtWasteTreatment: 0.5486,
+      districtDefault: 0.2652
+    }
+  }
+  */
+
+  // I dont know how to get the users selected site, as it is higher up in the hirarchy than the current snapshot.
+  const userTestValues = {
+    site: "8yancCd0U8IOTE6VzBEa",
+    householdSize: 1
+  }
+
+  // Switch statement based on the emission category. Only "heating", "transportation", and "electricity" should be valid options.
   switch (category) {
     case "heating":
-      carbonEmission = 1.5;
+      carbonEmission = value*1.5;
       break;
 
     case "transportation":
       carbonEmission =  1.0;
       break;
 
-    case "electricity":
-      carbonEmission =  0.5;
+    case "electricity": {
+      // electricityEF is the "Emission Factor" for electricity. Takes the appropriate value based on the user's site from "sites[site].electricity"
+      const electricityEF = sites[userTestValues.site as keyof typeof sites].electricity
+      // calculation for the carbon emission. Simply takes the entered kWh value, divided by the number of people in the household, times the electricity emission factor.
+      const householdSize = userTestValues.householdSize
+      carbonEmission = (value / householdSize) * electricityEF;
       break;
+    }
+      
       
     default:
       break;
