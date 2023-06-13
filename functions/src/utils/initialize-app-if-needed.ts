f47361a92a3b3c22969ac43968c2dc5bff7c0b3d
Sven Tiigi <sven.tiigi@gmail.com>
2023-06-13 15:09:03 +0200

    Set firestore settings once

--- functions/src/utils/initialize-app-if-needed.ts
+++ functions/src/utils/initialize-app-if-needed.ts
@@ -1,9 +1,16 @@
 import { App, getApps, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
 /**
  * Initializes the Firebase Admin SDK, if needed.
  */
 export function initializeAppIfNeeded(): App {
   const apps = getApps();
-  return apps.length === 0 ? initializeApp() : apps[0];
+  if (apps.length === 0) {
+    const app = initializeApp();
+    getFirestore().settings({ ignoreUndefinedProperties: true });
+    return app;
+  } else {
+    return apps[0];
+  }
 }
