c71806ded8352c1041e1770dae5d695075c05cda
Sven Tiigi <sven.tiigi@gmail.com>
2023-06-14 08:56:05 +0200

    Added comments

--- functions/src/utils/initialize-app-if-needed.ts
+++ functions/src/utils/initialize-app-if-needed.ts
@@ -5,12 +5,21 @@ import { getFirestore } from "firebase-admin/firestore";
  * Initializes the Firebase Admin SDK, if needed.
  */
 export function initializeAppIfNeeded(): App {
+  // Retrieve apps
   const apps = getApps();
+  // Check if no apps are available
   if (apps.length === 0) {
+    // Initialize app
     const app = initializeApp();
+    // Globally configure Firestore once to ignore undefined properties.
+    // Firestore is not capable of handling undefined values as only null values are allowed.
+    // But TypeScript prefers the usage of undefined over null!
+    // Read more: https://github.com/Microsoft/TypeScript/wiki/Coding-guidelines#null-and-undefined
     getFirestore().settings({ ignoreUndefinedProperties: true });
+    // Return app
     return app;
   } else {
+    // Return first app
     return apps[0];
   }
 }
