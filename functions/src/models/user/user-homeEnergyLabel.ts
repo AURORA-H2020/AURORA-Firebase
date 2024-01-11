1e2b702062f0cdb8daf1ab605184bf0651f0406e
LarsLrn <rotambus@googlemail.com>
2024-01-11 19:48:58 +0100

    Updated "User" type to latest specification

---
+++ functions/src/models/user/user-homeEnergyLabel.ts
@@ -0,0 +1,13 @@
+/**
+ * A home energy label of a user
+ */
+export type UserHomeEnergyLabel =
+    | "A+"
+    | "A"
+    | "B"
+    | "C"
+    | "D"
+    | "E"
+    | "F"
+    | "G"
+    | "unsure";
