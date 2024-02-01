42700bc12b631bc82b8d8db9264299ac87fc6908
LarsLrn <rotambus@googlemail.com>
2024-02-01 18:37:36 +0100

    Updated configuration to support emulators
    - Added support for firebase storage
    - Added storage rules

--- functions/src/utils/initialize-app-if-needed.ts
+++ functions/src/utils/initialize-app-if-needed.ts
@@ -1,5 +1,6 @@
 import { App, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from "firebase-admin/storage";
 
 /**
  * Initializes the Firebase Admin SDK, if needed.
@@ -11,11 +12,25 @@ export function initializeAppIfNeeded(): App {
   if (apps.length === 0) {
     // Initialize app
     const app = initializeApp();
-    // Globally configure Firestore once to ignore undefined properties.
-    // Firestore is not capable of handling undefined values as only null values are allowed.
-    // But TypeScript prefers the usage of undefined over null!
-    // Read more: https://github.com/Microsoft/TypeScript/wiki/Coding-guidelines#null-and-undefined
-    getFirestore().settings({ ignoreUndefinedProperties: true });
+
+    // Configure Firestore for emulator if running in emulator environment
+    if (process.env.FUNCTIONS_EMULATOR) {
+      getFirestore().settings({
+        host: "localhost:8080",
+        ssl: false,
+        ignoreUndefinedProperties: true,
+      });
+    } else {
+      // Globally configure Firestore once to ignore undefined properties.
+      // Firestore is not capable of handling undefined values as only null values are allowed.
+      // But TypeScript prefers the usage of undefined over null!
+      // Read more: https://github.com/Microsoft/TypeScript/wiki/Coding-guidelines#null-and-undefined
+      getFirestore().settings({ ignoreUndefinedProperties: true });
+    }
+
+    // Initialize Firebase Storage
+    getStorage();
+
     // Return app
     return app;
   } else {
