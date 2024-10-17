402ce4b95a5949e452c1f9c93e9871eec7b0da41
LarsLrn <rotambus@googlemail.com>
2024-10-17 18:48:00 +0200

    Updated types for PV preview feature

--- functions/src/models/country/city/country-city.ts
+++ functions/src/models/country/city/country-city.ts
@@ -6,4 +6,33 @@ export interface CountryCity {
    * The name
    */
   name: string;
+  /**
+   * Whether the PV feature is enabled
+   */
+  hasPhotovoltaics?: boolean;
+  /**
+   * Parameters for pvgis API
+   */
+  pvgisParams?: {
+    /**
+     * The latitude
+     */
+    lat: number;
+    /**
+     * The longitude
+     */
+    long: number;
+    /**
+     * The angle
+     */
+    angle: number;
+    /**
+     * The aspect
+     */
+    aspect: number;
+    /**
+     * The investment factor
+     */
+    investmentFactor: number;
+  };
 }
