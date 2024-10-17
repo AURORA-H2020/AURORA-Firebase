f17ee3db5194f7e3041194c6708dd0e26a1d1977
LarsLrn <rotambus@googlemail.com>
2024-10-17 18:51:16 +0200

    Added type for user pv investment

---
+++ functions/src/models/user/user-pv-investment/user-pv-investment.ts
@@ -0,0 +1,36 @@
+import { Timestamp } from "firebase-admin/firestore";
+
+export interface UserPvInvestment {
+  /**
+   * The user's investment in the installation
+   */
+  investment: number;
+  /**
+   * The user's share in the installation
+   */
+  share: number;
+  /**
+   * The user's investment date
+   */
+  investmentDate: Timestamp;
+  /**
+   * The city the invested installation is located in
+   */
+  city: string;
+  /**
+   * The PV installation ID
+   */
+  pvPlant: string;
+  /**
+   * The user's note about the investment
+   */
+  note?: string;
+  /**
+   * The time the investment was created
+   */
+  createdAt: Timestamp;
+  /**
+   * The time the investment was last updated
+   */
+  updatedAt: Timestamp;
+}
